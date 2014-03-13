aed6717d5ff99c54fbd8a041ec585e726ed64b91
if12b031-Ziffer <if12b031@technikum-wien.at>
2014-03-13 12:19:44 +0100

    GUI modified, fxml-controller connection implemented

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,10 @@
 package application;
-	
-import java.io.File;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 
 
 public class Main extends Application {
--- src/application/MainController.java
+++ src/application/MainController.java
@@ -9,20 +9,73 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
 public class MainController implements Initializable {
 
-	@FXML private Button button1;
-	@FXML private Label label1;
+	@FXML private Button buttonFileDecrypt;
+	@FXML private Button buttonGenPair;
+	@FXML private Button buttonFileEncrypt;
+	@FXML private Button buttonPKEncrypt;
+	@FXML private Button buttonLoadPrivate;
+	@FXML private Button buttonLoadPublic;
+	@FXML private Button decrypt;
+	@FXML private Button encrypt;
+	@FXML private Button loadKeyPair;
+	@FXML private Button generateKeyPair;
+	
+	Keys myKeys = new Keys();
+	CryptLogic myCryptLogic = new CryptLogic();
+	
+	
+	@FXML
+	public void decrypt(){
+		
+		String fileWithPath;
+		fileWithPath = buttonFileDecrypt.getText();
+		System.out.println("decrypt "+fileWithPath);
+		
+		
+	}
+	
+	@FXML
+	public void encrypt(){
+		String fileWithPath;
+		String publicKeyPath;
+		
+		fileWithPath = buttonFileEncrypt.getText();
+		publicKeyPath = buttonPKEncrypt.getText();
+		System.out.println("encrypt "+fileWithPath+" with "+publicKeyPath);
+	}
+	
+	@FXML
+	public void genKeyPair(){
+		
+		String savingPath;
+		
+		savingPath = buttonGenPair.getText();
+		
+		System.out.println("generate Key-Pair in "+savingPath);
+	}
+	
+	@FXML
+	public void loadKeyPair(){
+		String publicKeyPath;
+		String privateKeyPath;
+		
+		publicKeyPath = buttonLoadPublic.getText();
+		privateKeyPath = buttonLoadPrivate.getText();
+		
+		System.out.println("load public Key from "+publicKeyPath);
+		System.out.println("load private Key from "+privateKeyPath);	
+	}
+	
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 
-		button1.setText("Browse File");
-
-		button1.setOnAction(new EventHandler<ActionEvent>() {
+		buttonFileDecrypt.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 
 			public void handle(ActionEvent event) {
@@ -37,8 +90,100 @@ public class MainController implements Initializable {
 		          File file = fileChooser.showOpenDialog(null);
 		          if(file!=null)
 		        	  
-		              label1.setText(file.getPath());
+		        	  buttonFileDecrypt.setText(file.getPath());
 		          }
-		 });		
+		 });
+		
+		buttonGenPair.setOnAction(new EventHandler<ActionEvent>() {//directory search
+            @Override
+            public void handle(ActionEvent event) {
+                DirectoryChooser directoryChooser = new DirectoryChooser();
+                directoryChooser.setTitle("Selcet Directory");
+                //Show open file dialog
+                File file = directoryChooser.showDialog(null);
+               if(file!=null){
+            	   
+                    buttonGenPair.setText(file.getPath());
+               }
+            }
+
+});
+		
+		buttonFileEncrypt.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+
+			public void handle(ActionEvent event) {
+
+				 FileChooser fileChooser = new FileChooser();	
+				 fileChooser.setTitle("Select File");
+				 //Set extension filter
+		         FileChooser.ExtensionFilter extFilter =
+		         new FileChooser.ExtensionFilter("*", "*");
+		         fileChooser.getExtensionFilters().add(extFilter);
+		          //Show open file dialog
+		          File file = fileChooser.showOpenDialog(null);
+		          if(file!=null)
+		        	  
+		        	  buttonFileEncrypt.setText(file.getPath());
+		          }
+		 });
+		
+		buttonPKEncrypt.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+
+			public void handle(ActionEvent event) {
+
+				 FileChooser fileChooser = new FileChooser();	
+				 fileChooser.setTitle("Select Public-Key");
+				 //Set extension filter
+		         FileChooser.ExtensionFilter extFilter =
+		         new FileChooser.ExtensionFilter("*", "*");
+		         fileChooser.getExtensionFilters().add(extFilter);
+		          //Show open file dialog
+		          File file = fileChooser.showOpenDialog(null);
+		          if(file!=null)
+		        	  
+		        	  buttonPKEncrypt.setText(file.getPath());
+		          }
+		 });
+		
+		buttonLoadPrivate.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+
+			public void handle(ActionEvent event) {
+
+				 FileChooser fileChooser = new FileChooser();	
+				 fileChooser.setTitle("Select Private-Key");
+				 //Set extension filter
+		         FileChooser.ExtensionFilter extFilter =
+		         new FileChooser.ExtensionFilter("*", "*");
+		         fileChooser.getExtensionFilters().add(extFilter);
+		          //Show open file dialog
+		          File file = fileChooser.showOpenDialog(null);
+		          if(file!=null)
+		        	  
+		        	  buttonLoadPrivate.setText(file.getPath());
+		          }
+		 });
+		
+		buttonLoadPublic.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+
+			public void handle(ActionEvent event) {
+
+				 FileChooser fileChooser = new FileChooser();	
+				 fileChooser.setTitle("Select Public-Key");
+				 //Set extension filter
+		         FileChooser.ExtensionFilter extFilter =
+		         new FileChooser.ExtensionFilter("*", "*");
+		         fileChooser.getExtensionFilters().add(extFilter);
+		          //Show open file dialog
+		          File file = fileChooser.showOpenDialog(null);
+		          if(file!=null)
+		        	  
+		        	  buttonLoadPublic.setText(file.getPath());
+		          }
+		 });
+		
 	}
 }
